49855cf56261556716510959dba37eac117821f0
Daniel Plucenio <dplucenio@gmail.com>
2016-03-31 19:12:41 -0300

    First commit. Pure python, cython and parallel cython implementation

---
+++ heat_diffusion_experiment/__init__.py
@@ -0,0 +1,2 @@
+from .cython_versions import cython_version, cython_parallel_version, show
+from .python_version import python_version
---
+++ heat_diffusion_experiment/python_version.py
@@ -0,0 +1,36 @@
+import numpy as np
+from builtins import range
+
+def python_version(M, total_time=500.0):
+    
+    # Physical properties --------------------------------------------------------------------------------
+    k = 385.0
+    rho = 8000.0
+    cp = 400.0;
+    
+    # Geometrical properties -----------------------------------------------------------------------------
+    L = 1.0
+    l = L/M
+    A = l**2
+    V = l**3.0        
+    
+    # Boundary conditions --------------------------------------------------------------------------------
+    u = np.zeros((M,M),dtype=np.float64) # Initial temperature
+    for i in range(M):
+        u[M-1,i] = 25
+    
+    u[M-1,:] = 25 # Temperature at top wall
+    
+    # , 'necessary dt: %f'%((0.9 * rho * cp * (l ** 2) / (4.0 * k)))
+    dt = (0.9 * rho * cp * (l ** 2) / (4.0 * k))
+
+    f = k * A / l # Diffusion term
+    B = rho * cp * V / dt # Transient term
+
+    num_timesteps = int(total_time/dt)
+    for t in range(num_timesteps):
+        u0 = np.copy(u)        
+        for i in range(1,M-1):
+            for j in range(1,M-1):
+                u[i,j] = (f*u0[i,j+1] + f*u0[i,j-1] + f*u0[i+1,j] + f*u0[i-1,j] + B*u0[i,j]) / (B + 4*f)
+    return u
