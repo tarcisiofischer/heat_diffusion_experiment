af2ef6550d8b1e3913ad1bf73ce53a5aff278d62
Tarcisio Fischer <tarcisio@esss.com.br>
2016-07-01 15:26:06 -0300

    Simplify simulation code implementation

--- solver/heat_diffusion_2d.py
+++ solver/heat_diffusion_2d.py
@@ -56,75 +56,32 @@ def solve(
     for t in range(num_timesteps):
         u0 = np.copy(u)
 
-        # Solve boundaries ------------------------------------------------------------------------
-        u[0, 0] = (
-            f * u0[0, 1] +
-            f / 2.0 * left_temperature +
-            f * u0[1, 0] +
-            f / 2.0 * bottom_temperature +
-            B * u0[0, 0]
-        ) / (B + 4 * f)
-        u[0, n_elements - 1] = (
-            f / 2.0 * right_temperature +
-            f * u0[0, n_elements - 2] +
-            f * u0[1, n_elements - 1] +
-            f / 2.0 * bottom_temperature +
-            B * u0[0, n_elements - 1]
-        ) / (B + 4 * f)
-        u[n_elements - 1, 0] = (
-            f * u0[n_elements - 1, 1] +
-            f / 2.0 * left_temperature +
-            f / 2.0 * top_temperature +
-            f * u0[n_elements - 2, 0] +
-            B * u0[n_elements - 1, 0]
-        ) / (B + 4 * f)
-        u[n_elements - 1, n_elements - 1] = (
-            f / 2.0 * right_temperature +
-            f * u0[n_elements - 1, n_elements - 2] +
-            f / 2.0 * top_temperature +
-            f * u0[n_elements - 2, n_elements - 1] +
-            B * u0[n_elements - 1, n_elements - 1]
-        ) / (B + 4 * f)
-        for i in range(1, n_elements - 1):
-            u[i, 0] = (
-                f * u0[i, 1] +
-                f / 2.0 * left_temperature +
-                f * u0[i + 1, 0] +
-                f * u0[i - 1, 0] +
-                B * u0[i, 0]
-            ) / (B + 4 * f)
-            u[i, n_elements - 1] = (
-                f / 2.0 * right_temperature +
-                f * u0[i, n_elements - 2] +
-                f * u0[i + 1, n_elements - 1] +
-                f * u0[i - 1, n_elements - 1] +
-                B * u0[i, n_elements - 1]
-            ) / (B + 4 * f)
-            u[0, i] = (
-                f * u0[0, i + 1] +
-                f * u0[0, i - 1] +
-                f * u0[1, i] +
-                f / 2.0 * bottom_temperature +
-                B * u0[0, i]
-            ) / (B + 4 * f)
-            u[n_elements - 1, i] = (
-                f * u0[n_elements - 1, i + 1] +
-                f * u0[n_elements - 1, i - 1] +
-                f / 2.0 * top_temperature +
-                f * u0[n_elements - 2, i] +
-                B * u0[n_elements - 1, i]
-            ) / (B + 4 * f)
+        for i in range(0, n_elements):
+            for j in range(0, n_elements):
+                u[i, j] = 0.0
 
-        # Solve middle ----------------------------------------------------------------------------
-        for i in range(1, n_elements - 1):
-            for j in range(1, n_elements - 1):
-                u[i, j] = (
-                    f * u0[i, j + 1] +
-                    f * u0[i, j - 1] +
-                    f * u0[i + 1, j] +
-                    f * u0[i - 1, j] +
-                    B * u0[i, j]
-                ) / (B + 4 * f)
+                if j + 1 <= n_elements - 1:
+                    u[i, j] += f * u0[i, j + 1]
+                else:
+                    u[i, j] += f / 2.0 * right_temperature
+
+                if j - 1 >= 0:
+                    u[i, j] += f * u0[i, j - 1]
+                else:
+                    u[i, j] += f / 2.0 * left_temperature
+
+                if i + 1 <= n_elements - 1:
+                    u[i, j] += f * u0[i + 1, j]
+                else:
+                    u[i, j] += f / 2.0 * top_temperature
+
+                if i - 1 >= 0:
+                    u[i, j] += f * u0[i - 1, j]
+                else:
+                    u[i, j] += f / 2.0 * bottom_temperature
+
+                u[i, j] += B * u0[i, j]
+                u[i, j] /= B + 4 * f
         if after_timestep_callback:
             after_timestep_callback(u, t * dt)
 
